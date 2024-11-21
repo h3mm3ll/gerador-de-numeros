c14897f619a8b122ce72e4d344e4227e5fccaa0f
Luiza Hemmel Piovesan <hemmelpiovesanl@gmail.com>
2024-11-21 19:56:03 -0300

    fiz um gerador de números aleatórios

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,15 @@
-const button =document.getElementById("")
+const button =document.getElementById("genarate");
+
+button.addEventListener('click',function(){
+        const min = parseInt(document.getElementById('min').value);
+        const max = parseInt(document.getElementById('max').value);
+
+        let result = Math.floor(Math.random() * (max - min + 1))+min;
+
+            if (isNaN(result)){
+                result = 'valor inválido'
+            }
+
+        document.querySelector('#result > span').textContent = result
+
+});
